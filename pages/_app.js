482233a3478a289392b3a8b23584050374993e89
MezianeKhalil <khalilmez2000@gmail.com>
2020-12-08 21:45:09 +0100

    final touch 0.2

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,9 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="title" content="InfoCourse"/>
         <meta name="description" content="InfoCourse is a library of Courses, Exercises, Exams and several other documents for all UHBC informatique students"/>
-        <meta property="og:type" content="website"/>
+        <meta property="og:type" content="documentation"/>
         <meta property="og:description" content="InfoCourse is a library of Courses, Exercises, Exams for all UHBC informatique students"/>
-        <meta property="og:image" content="https://infocourse.vercel.app/public/couver.png"/>
+        <meta property="og:image" content="https://infocourse.vercel.app/couver.png"/>
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://infocourse.vercel.app"/>
         <meta property="twitter:title" content="InfoCourse"/>
