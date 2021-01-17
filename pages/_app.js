92edb349b2daa388cc1b574e0f577ccc6e04081e
MezianeKhalil <khalilmez2000@gmail.com>
2021-01-17 22:31:14 +0100

    add Google Analytics for tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import Head from 'next/head'
 import '../styles/tailwind.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return(
     <>
       <Head>
