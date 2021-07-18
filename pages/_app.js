120a5a1c9319e274093e49f5527811c91a7a51e6
MezianeKhalil <khalilmez2000@gmail.com>
2021-07-18 22:11:20 +0100

    add new_thumbnail_design

--- pages/_app.js
+++ pages/_app.js
@@ -21,12 +21,12 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="InfoCourse is a library of Courses, Exercises, Exams and several other documents for all UHBC informatique students"/>
         <meta property="og:type" content="documentation"/>
         <meta property="og:description" content="InfoCourse is a library of Courses, Exercises, Exams for all UHBC informatique students"/>
-        <meta property="og:image" content="https://infocourse.vercel.app/couver.png"/>
+        <meta property="og:image" content="https://infocourse.vercel.app/new_thumbnail_design.png"/>
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://infocourse.vercel.app"/>
         <meta property="twitter:title" content="InfoCourse"/>
         <meta property="twitter:description" content="InfoCourse is a library of Courses, Exercises, Exams and several other documents for all UHBC informatique students"/>
-        <meta property="twitter:image" content="https://infocourse.vercel.app/public/couver.png"/>
+        <meta property="twitter:image" content="https://infocourse.vercel.app/public/new_thumbnail_design.png"/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins" rel="stylesheet"/>
       </Head>
       <Component {...pageProps} />
